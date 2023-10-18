83e55c9344f9e4d20d32aeb6cb5bd1e9e786c68d
Leon <leon.wuuu@gmail.com>
2023-10-19 10:11:47 +1100

    added scrolling animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={leon} alt="logo" className="w-20 h-20 object-contain" />
+          <img src={leon} alt="logo" className="w-9 h-9 object-contain" />
 
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Leon &nbsp; 
