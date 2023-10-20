18e68d1a08df3f8130ed54fc162b149bd6bf29f5
Leon <leon.wuuu@gmail.com>
2023-10-20 11:33:18 +1100

    added experience section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,10 @@ import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
 
+
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt className="xs:w-[250px] w-full" options={{reverse: true}}>
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
