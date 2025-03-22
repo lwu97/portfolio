433bfa53231bf8f76ef97843a4f32b0a27afe1ed
Leon Wu <leon.wuuu@gmail.com>
2025-03-22 17:33:13 +1100

    update font and styles

--- src/styles.js
+++ src/styles.js
@@ -2,17 +2,16 @@ const styles = {
     paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
-  
+
     heroHeadText:
-      "font-black text-white lg:text-[40px] sm:text-[30px] xs:text-[20px] text-[40px] lg:leading-[98px] mt-2",
+        "font-black text-beige lg:text-[40px] sm:text-[30px] xs:text-[20px] text-[40px] lg:leading-[98px] mt-2",
     heroSubText:
-      "text-white font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
-  
+        "text-beige lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+
     sectionHeadText:
-      "text-white font-black font-[mursgothic-widedark] font-bold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+        "text-beige font-black font-mon font-bold md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionSubText:
-      "sm:text-[18px] text-white text-[10px] font-[VelaSansBold] font-bold text-secondary uppercase tracking-wider",
-  };
-  
-  export { styles };
-  
+        "sm:text-[18px] text-beige text-[10px] font-mon font-bold text-secondary uppercase tracking-wider",
+};
+
+export { styles };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#191919",
-        secondary: "#d3d2d6",
-        tertiary: "#444444",
-        "black-100": "#100d25",
-        "black-200": "#090325",
-        "white-100": "#f3f3f3",
-      },
-      boxShadow: {
-        card: "0px 35px 120px -15px #211e35",
-      },
-      screens: {
-        xs: "450px",
-      },
-      backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg.png')",
-      },
-      
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            fontFamily: {
+                mon: ["MonumentGrotesk-Regular", "sans-serif"], // Default to your custom font
+            },
+            colors: {
+                primary: "#191919",
+                secondary: "#d3d2d6",
+                tertiary: "#444444",
+                "black-100": "#100d25",
+                "black-200": "#090325",
+                "white-100": "#f3f3f3",
+                green: "#b2d74c",
+                beige: "#E4E5DF",
+            },
+            boxShadow: {
+                card: "0px 35px 120px -15px #211e35",
+            },
+            screens: {
+                xs: "450px",
+            },
+            backgroundImage: {
+                "hero-pattern": "url('/src/assets/herobg.png')",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
